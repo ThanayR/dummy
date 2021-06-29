834a1115e11fb1bf12221e69189c94dff1f69a94
ThanayR <email2sravi@yahoo.co.in>
2021-06-29 11:21:50 +0530

    1st Commit

---
+++ dummy/src/demo_enum.java
@@ -0,0 +1,14 @@
+
+public class demo_enum {
+	//public static final 
+	enum weekDays {Today_is_Monday, Tuesday, Wednesday, Four};	
+	
+	public static void main(String[] args) {
+		weekDays wd = weekDays.Tuesday;
+		// System.out.println(wd);	
+	}
+	
+	public static int main2() {
+		return 11;	
+	}	
+}
